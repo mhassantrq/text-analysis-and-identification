2a2796bd759fdc493c304ae1af99e7c9c5c5a435
mhassantrq <mhassantrq@gmail.com>
2026-07-02 19:26:52 +0500

    added lower casing

--- preprocessing.py
+++ preprocessing.py
@@ -21,8 +21,26 @@ def train_test_split(data, train_size=0.8):
     test_data_generated = generated_rows.iloc[train_size_generated:]
     test_data_written = written_rows.iloc[train_size_written:]
 
+    train_data_generated = train_data_generated['text']
+    train_data_written = train_data_written['text']
+
+    test_data_generated = test_data_generated['text']
+    test_data_written = test_data_written['text']
+
     return train_data_generated, train_data_written, test_data_generated, test_data_written
+
+def lower_case(data):
+    for i in range(len(data)):
+        data.iloc[i] = data.iloc[i].lower()
+    return data
 
 
 data = read_csv()
+
 train_data_generated, train_data_written, test_data_generated, test_data_written = train_test_split(data)
+
+train_data_generated = lower_case(train_data_generated)
+train_data_written = lower_case(train_data_written)
+
+test_data_generated = lower_case(test_data_generated)
+test_data_written = lower_case(test_data_written)
