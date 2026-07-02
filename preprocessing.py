7db3a53480fc0c896e71b17868076ea2429345c0
mhassantrq <mhassantrq@gmail.com>
2026-07-02 19:00:54 +0500

    added train test split in preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,10 +1,28 @@
 from read_data import read_csv
+from collections import defaultdict
 
-df = read_csv()
 
-data = []
+def train_test_split(data, train_size=0.8):
+    generated_rows = data[data['label'] == 1]
+    written_rows = data[data['label'] == 0]
+    
+    # generated_rows = generated_rows.sample(frac=1)
+    # written_rows = written_rows.sample(frac=1)
 
-for row in df['text']:
-    data.append(row.lower())
+    train_data = []
+    test_data = []
 
-print(data[:2])
+    train_size_generated = int(len(generated_rows) * train_size)
+    train_size_written = int(len(written_rows) * train_size)
+
+    train_data_generated = generated_rows.iloc[:train_size_generated]
+    train_data_written = written_rows.iloc[:train_size_written]
+
+    test_data_generated = generated_rows.iloc[train_size_generated:]
+    test_data_written = written_rows.iloc[train_size_written:]
+
+    return train_data_generated, train_data_written, test_data_generated, test_data_written
+
+
+data = read_csv()
+train_data_generated, train_data_written, test_data_generated, test_data_written = train_test_split(data)
