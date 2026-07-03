5850742c7df0c58a9cad03cef19d8674229808b6
mhassantrq <mhassantrq@gmail.com>
2026-07-03 13:40:00 +0500

    removed bag of words from preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,3 @@
-import read_data
-from collections import defaultdict
-
-
 def split_train_test(data, train_size=0.8):
     generated_rows = data[data['label'] == 1]
     written_rows = data[data['label'] == 0]
@@ -40,31 +36,4 @@ def split_features(data):
         data.iloc[i] = data.iloc[i].split()
     return data
 
-def bag_of_words(data):
-    bag = defaultdict(int)
-    for doc in data:
-        for w in doc:
-            bag[w] += 1    
-    return bag
 
-
-data = read_data.read_csv()
-
-train_data_generated, train_data_written, test_data_generated, test_data_written = split_train_test(data)
-
-train_data_generated = lower_case(train_data_generated)
-train_data_written = lower_case(train_data_written)
-
-test_data_generated = lower_case(test_data_generated)
-test_data_written = lower_case(test_data_written)
-
-train_data_generated = split_features(train_data_generated)
-train_data_written = split_features(train_data_written)
-
-test_data_generated = split_features(test_data_generated)
-test_data_written = split_features(test_data_written)
-
-
-
-tg = bag_of_words(train_data_generated)
-print(tg['the'])
