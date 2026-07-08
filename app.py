f51c78ae88c447a7cbb655d2596b30cd321d78b4
mhassantrq <mhassantrq@gmail.com>
2026-07-08 23:14:09 +0500

    added form, check live text from model

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import joblib
+
 
 app = Flask(__name__, template_folder='templates')
 
-@app.route('/')
+nb_model = joblib.load('models/nb.pkl')
+count_vec = joblib.load('models/nb_vectorizer.pkl')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    predictions = None
+
+    if request.method == 'POST':
+
+        text = request.form['text']
+        vec = count_vec.transform([text])
+        prb = nb_model.predict_proba(vec)[0]
+        pred = nb_model.predict(vec)[0]
+
+        predictions = {
+            'pred' : pred,
+            'written' : prb[0],
+            'generated' : prb[1],
+        }
+
+    return render_template('index.html', predictions=predictions)
 
 
 if __name__ == '__main__':
